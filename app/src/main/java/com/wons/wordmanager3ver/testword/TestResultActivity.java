ab08928b41687eb42122b9125aa1343f9b80904b
wono9 <wono96@naver.com>
2022-04-03 23:11:35 +0900

    20220402

--- app/src/main/java/com/wons/wordmanager3ver/testword/TestResultActivity.java
+++ app/src/main/java/com/wons/wordmanager3ver/testword/TestResultActivity.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider;
 import android.os.Bundle;
 
 import com.wons.wordmanager3ver.databinding.ActivityTestResultBinding;
+import com.wons.wordmanager3ver.testword.adapter.ResultAdapter;
 
 public class TestResultActivity extends AppCompatActivity {
 
@@ -18,5 +19,46 @@ public class TestResultActivity extends AppCompatActivity {
         binding = ActivityTestResultBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         viewModel = new ViewModelProvider(this).get(TestResultActivityViewModel.class);
+        viewModel.initData();
+        viewModel.listIndexLiveData.observe(this, index -> {
+            setListView();
+        });
+        binding.btnChecked.setOnClickListener(v -> {
+
+        });
+
+        binding.btnBack.setOnClickListener( v -> {
+
+        });
+
+        binding.btnLeft.setOnClickListener(v -> {
+
+        });
+
+        binding.btnRight.setOnClickListener(v -> {
+
+        });
+
+    }
+
+    private void setResultListView() {
+
+    }
+
+    private void setResultText() {
+        binding.tvLanguage.setText(viewModel.getLanguage());
+        binding.tvListCountInResult.setText(viewModel.getListCountInResult());
+        binding.tvWordCount.setText(viewModel.getWordCount());
+        binding.tvCorrectCount.setText(viewModel.getCorrectWordCount());
+        binding.tvPass.setText();
+    }
+
+    private void setListView() {
+        if(binding.lvWord.getAdapter() == null) {
+            binding.lvWord.setAdapter(new ResultAdapter());
+        }
+        ((ResultAdapter)binding.lvWord.getAdapter()).setArrayList(viewModel.getTestResult());
+        ((ResultAdapter)binding.lvWord.getAdapter()).setWordInfoHashMap(viewModel.getMapByNowList());
+        ((ResultAdapter)binding.lvWord.getAdapter()).notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/wons/wordmanager3ver/testword/TestResultActivityViewModel.java
+++ app/src/main/java/com/wons/wordmanager3ver/testword/TestResultActivityViewModel.java
@@ -1,6 +1,105 @@
 package com.wons.wordmanager3ver.testword;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.wons.wordmanager3ver.MainViewModel;
+import com.wons.wordmanager3ver.MyDao;
+import com.wons.wordmanager3ver.datavalues.EnumLanguage;
+import com.wons.wordmanager3ver.datavalues.TestWordResult;
+import com.wons.wordmanager3ver.datavalues.TodayWordList;
+import com.wons.wordmanager3ver.datavalues.WordInfo;
+import com.wons.wordmanager3ver.datavalues.WordList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class TestResultActivityViewModel extends ViewModel {
+
+    public MutableLiveData<Integer> listIndexLiveData;
+    private ArrayList<TodayWordList> todayWordLists;
+    private HashMap<Integer, ArrayList<TestWordResult>> resultWordMap;
+    private MyDao dao = MainViewModel.dao;
+
+    public void setListIndexLiveData(int index) {
+        if(this.listIndexLiveData == null) {
+            this.listIndexLiveData = new MutableLiveData<>();
+        }
+        this.listIndexLiveData.setValue(index);
+    }
+
+    public int getListIndexLiveData() {
+        if(this.listIndexLiveData == null || this.listIndexLiveData.getValue() == null) {
+            this.listIndexLiveData = new MutableLiveData<>();
+            listIndexLiveData.setValue(0);
+        }
+
+        return listIndexLiveData.getValue();
+    }
+
+    public void initData() {
+        if(listIndexLiveData == null) {
+            this.listIndexLiveData = new MutableLiveData<>();
+        }
+        TestWordResult[] testWordResults = dao.getAllTestWordResult();
+        TodayWordList[] todayWordLists = dao.getAllTodayListByLanguageCode(MainViewModel.getUserInfo().getLanguageCode());
+        this.todayWordLists = new ArrayList<>(Arrays.asList(todayWordLists));
+        for(int i=0 ; i<todayWordLists.length ; i++) {
+            ArrayList<TestWordResult> testWordResults1 = new ArrayList<>();
+            for (TestWordResult testWordResult : testWordResults) {
+                if (testWordResult.getListCodeOfWord() == todayWordLists[i].getListCode()) {
+                    testWordResults1.add(testWordResult);
+                }
+            }
+            if (resultWordMap == null) {
+                this.resultWordMap = new HashMap<>();
+            }
+            this.resultWordMap.put(i, testWordResults1);
+        }
+    }
+
+    public String getLanguage() {
+        return EnumLanguage.ENGLISH.getLanguage(MainViewModel.getUserInfo().getLanguageCode());
+    }
+
+    public String getListCount() {
+        return getListIndexLiveData() + 1 + "/" + todayWordLists.size();
+    }
+
+    public String getListCountInResult() {
+        return String.valueOf(todayWordLists.size());
+    }
+
+    public String getWordCount() {
+        return String.valueOf(dao.getAllTestWordResult().length);
+    }
+
+    public String getCorrectWordCount() {
+        int correctCount = 0;
+        TestWordResult[] testWordResults = dao.getAllTestWordResult();
+        for(TestWordResult testWordResult : testWordResults) {
+            if(testWordResult.getTestResult()) {
+                correctCount++;
+            }
+        }
+        return correctCount + "/" + testWordResults.length;
+    }
+
+    public HashMap<String, WordInfo> getMapByNowList() {
+        int nowIndex = getListIndexLiveData();
+        ArrayList<TestWordResult> testWordResults = resultWordMap.get(todayWordLists.get(nowIndex));
+        HashMap<String, WordInfo> map = new HashMap<>();
+
+        for(TestWordResult testWordResult : testWordResults) {
+            WordInfo wordInfo = dao.getWordInfo(testWordResult.getWordTitle().toUpperCase(), MainViewModel.getUserInfo().getLanguageCode());
+            map.put(testWordResult.getWordTitle().toUpperCase(), wordInfo);
+        }
+        return map;
+    }
+
+    public ArrayList<TestWordResult> getTestResult() {
+        return resultWordMap.get(getListIndexLiveData());
+    }
 }
+
