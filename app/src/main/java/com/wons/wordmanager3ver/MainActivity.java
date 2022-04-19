9ce6cba84ba369b90f51ecceb4f29730553b10d0
wono9 <wono96@naver.com>
2022-04-19 16:46:15 +0900

    test

--- app/src/main/java/com/wons/wordmanager3ver/MainActivity.java
+++ app/src/main/java/com/wons/wordmanager3ver/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e("test","Test");
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         BottomNavigationView navigationView = findViewById(R.id.btN);
